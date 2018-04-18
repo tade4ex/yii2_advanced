54b02d97f747a446082e1a85b2b3f743705a6acd
root <root@DESKTOP-IIE32N4.localdomain>
2018-04-18 20:47:28 +0200

    homework 7

--- frontend/views/layouts/left.php
+++ frontend/views/layouts/left.php
@@ -7,9 +7,9 @@
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
                     ['label' => Yii::t('app', 'Menu'), 'options' => ['class' => 'header']],
-                    ['label' => Yii::t('app', 'Dashboard'), 'url' => ['/site/index']],
-                    ['label' => Yii::t('app', 'Calendar'), 'url' => ['/site/calendar']],
-                    ['label' => Yii::t('app', 'Subscribe info'), 'url' => ['/site/subscribe']],
+                    ['label' => Yii::t('app', 'Dashboard'), 'url' => ['/site/index'], 'icon' => 'dashboard'],
+                    ['label' => Yii::t('app', 'Calendar'), 'url' => ['/site/calendar'], 'icon' => 'calendar'],
+                    ['label' => Yii::t('app', 'Subscribe info'), 'url' => ['/site/subscribe'], 'icon' => 'share'],
 //                    ['label' => Yii::t('app', 'Add Project'), 'url' => ['/project/create']],
                 ],
             ]
